15be264b8d699fbf0223c8dafc404d172e44d424
Joan_E <joanevans18@gmail.com>
2020-07-03 22:39:37 +0300

    added flask_script module

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask
 
 from flask_bootstrap import Bootstrap
+from config import config_options
+
 
 bootstrap = Bootstrap()
 
 def create_app(config_name):
+    # initialized the app
     app = Flask(__name__, instance_relative_config=True)
 
     # app configurations
+    app.config.from_object(config_options[config_name])
     
     # flask extensions
     bootstrap.init_app(app)
