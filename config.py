99dc87cf76829e0d4e33aa9e625743079403e590
Joan_E <joanevans18@gmail.com>
2020-07-04 16:17:27 +0300

    edited the base_url

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import os
 
 class Config:
-    pass
+    
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
 class ProdConfg(Config):
     """
