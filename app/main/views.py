28a045fbad6e5a45be4477d9a51324dc278d4029
Joan_E <joanevans18@gmail.com>
2020-07-04 11:46:33 +0300

    Added /headline and /everything routes

--- app/main/views.py
+++ app/main/views.py
@@ -5,3 +5,14 @@ from . import main
 def index():
     title = 'Home'
     return render_template('index.html', title=title)
+
+@main.route('/headline')
+def headline():
+    title = 'Headline'
+    return render_template('headline.html', title=title)
+
+@main.route('/everything')
+def everything():
+    title = 'Everything'
+    return render_template('everything.html', title= title)
+
