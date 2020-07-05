262c2fb16178302d555491d868365c220779d121
Joan_E <joanevans18@gmail.com>
2020-07-05 18:10:17 +0300

    Created a route for specific search

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
 from . import main
-from ..requests import get_sources, get_leading_articles_by_source
+from ..requests import get_sources, get_leading_articles_by_source, search_article_keyword
+
 
 @main.route('/')
 def index():
@@ -16,11 +17,24 @@ def headline():
 @main.route('/everything')
 def everything():
     title = 'Everything'
-    return render_template('everything.html', title= title)
+    keyword = request.args.get('keyword')
+    language = request.args.get('language')
+    if keyword or language:
+        return redirect(url_for('main.everything_search', keyword=keyword, language=language))
+    else:
+        return render_template('everything.html', title= title)
+    
 
 @main.route('/headline/<source>')
 def headline_source(source):
     headlines = get_leading_articles_by_source(source)
-    print(headlines)
+    message= 'Headlines for %s' % source.capitalize()
+    return render_template('sourceHeadline.html', headlines = headlines, message = message)
 
-    return render_template('sourceHeadline.html', headlines = headlines)
+@main.route('/everything/article/<keyword>&<language>')
+def everything_search(keyword, language):
+    new_keyword = '+'.join(keyword.split(' '))
+    searched_articles = search_article_keyword(new_keyword, language)
+    message = "Here are the first 15 articles with %s" % keyword
+    return render_template('everything.html', searched_articles=searched_articles, message=message)
+
